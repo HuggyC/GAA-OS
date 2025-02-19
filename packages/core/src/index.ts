3965e0d9bbde7c0f5f3209321c337f433c6ec59b
HuggyC <103903174+HuggyC@users.noreply.github.com>
2025-02-19 15:53:37 +0100

    Add core package entry point

---
+++ packages/core/src/index.ts
@@ -0,0 +1,10 @@
+// Re-export components and utilities from ProzillaOS core
+export * from '@prozilla-os/core';
+
+// Export custom components for GAA-OS
+export * from './components';
+export * from './hooks';
+export * from './utils';
+
+// Export type definitions
+export * from './types';
